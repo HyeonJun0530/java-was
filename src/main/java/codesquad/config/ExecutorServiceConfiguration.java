11517c007456a83e2972bff61cddadbc18a85c3f
HyeonJun0530 <hyjn053@gmail.com>
2024-07-02 19:57:46 +0900

    feat: Thread Pool 구현 및 Main 수정
    - Thread Pool 생성해서 반환하는 config 구현
    - Main에서 Thread를 사용하도록 수정

---
+++ src/main/java/codesquad/config/ExecutorServiceConfiguration.java
@@ -0,0 +1,22 @@
+package codesquad.config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceConfiguration {
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 50;
+    private static final ExecutorService executorService = new ThreadPoolExecutor(
+            THREAD_POOL_SIZE,
+            THREAD_POOL_SIZE,
+            0L,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(QUEUE_CAPACITY)
+    );
+
+    public static ExecutorService getExecutorService() {
+        return executorService;
+    }
+}
