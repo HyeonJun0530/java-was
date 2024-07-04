6675c1289144e053225b97a7317c177b71538f7c
HyeonJun0530 <hyjn053@gmail.com>
2024-07-05 01:08:27 +0900

    feat: api 요청과 정적 요청을 다루는 handler 구현 및 Processor 리팩터링
    - Static 요청을 다루는 handler 구현
    - api 요청을 다루는 handler 구현
    - 요청을 핸들링하는 HttpRequestHandler 구현
    - HttpProcessor 리팩터링

---
+++ src/main/java/codesquad/http/handler/ApiHandler.java
@@ -0,0 +1,59 @@
+package codesquad.http.handler;
+
+import codesquad.app.api.UserApi;
+import codesquad.app.api.annotation.ApiMapping;
+import codesquad.http.message.constant.HttpStatus;
+import codesquad.http.message.request.HttpRequest;
+import codesquad.http.message.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+public class ApiHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiHandler.class);
+    private static final Map<String, Class> apiMapper = Map.of("/create", UserApi.class);
+
+    private ApiHandler() {
+    }
+
+    public static HttpResponse handle(final HttpRequest httpRequest) {
+        String path = httpRequest.getRequestStartLine().getPath();
+        Class<?> apiClass = apiMapper.get(path);
+
+        if (apiClass == null) {
+            return HttpResponse.of(httpRequest.getRequestStartLine().getProtocol(), HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            Constructor<?> constructor = apiClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object apiInstance = constructor.newInstance();
+
+            for (Method method : apiClass.getMethods()) {
+                if (method.isAnnotationPresent(ApiMapping.class)) {
+                    ApiMapping apiMapping = method.getAnnotation(ApiMapping.class);
+                    if (apiMapping.path().equals(path) && apiMapping.method().equals(httpRequest.getRequestStartLine().getMethod())) {
+                        HttpResponse response = (HttpResponse) method.invoke(apiInstance, httpRequest);
+                        return response;
+                    }
+                }
+            }
+
+            return HttpResponse.of(httpRequest.getRequestStartLine().getProtocol(), HttpStatus.NOT_FOUND);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            log.error("API handler error", e);
+            return HttpResponse.of(httpRequest.getRequestStartLine().getProtocol(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public static boolean isApiRequest(final String path) {
+        return apiMapper.containsKey(path);
+    }
+
+}
