977f878401275268ae41e3d38a2dc6b136a8c90a
HyeonJun0530 <hyjn053@gmail.com>
2024-07-05 01:00:08 +0900

    feat: Utils 클래스 추가
    - String 관련 유틸 클래스
    - HttpMessage Date 관련 유틸 클래스

---
+++ src/main/java/codesquad/utils/StringUtils.java
@@ -0,0 +1,24 @@
+package codesquad.utils;
+
+public final class StringUtils {
+
+    public static final String EMPTY = "";
+    public static final String SPACE = " ";
+    public static final String NEW_LINE = "\r\n";
+    public static final String COLON = ":";
+    public static final String SEMICOLON = ";";
+    public static final String COMMA = ",";
+    public static final String EQUAL = "=";
+    public static final String AMPERSAND = "&";
+    public static final String QUESTION_MARK = "?";
+    public static final String SLASH = "/";
+    public static final String DOT = ".";
+    public static final String UNDERSCORE = "_";
+    public static final String HYPHEN = "-";
+    public static final String ASTERISK = "*";
+    public static final String AND = "&";
+
+
+    private StringUtils() {
+    }
+}
