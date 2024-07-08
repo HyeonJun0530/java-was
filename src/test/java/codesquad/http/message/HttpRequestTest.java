43d129ac1b63db81a229fcb308ee4d085a8517a4
HyeonJun0530 <hyjn053@gmail.com>
2024-07-08 21:58:48 +0900

    test: HttpRequest 테스트 케이스 추가 및 테스트 리팩터링
    - 쿼리 스트링 테스트 추가
    - 실패지점 빠른 확인을 위한 assertAll 추가

--- src/test/java/codesquad/http/message/HttpRequestTest.java
+++ src/test/java/codesquad/http/message/HttpRequestTest.java
@@ -10,8 +10,14 @@ import java.io.IOException;
 import java.io.StringReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class HttpRequestTest {
+
+    private static HttpRequest getHttpRequest(final String input) throws IOException {
+        HttpRequest from = HttpRequest.from(new BufferedReader(new StringReader(input)));
+        return from;
+    }
 
     @Test
     @DisplayName("HttpRequest 객체 생성 테스트 - body가 없는 경우")
@@ -21,12 +27,14 @@ class HttpRequestTest {
                 "Content-Type: text/html\r\n" +
                 "Content-Length: 0\r\n";
 
-        HttpRequest from = HttpRequest.from(new BufferedReader(new StringReader(input)));
+        HttpRequest from = getHttpRequest(input);
 
-        assertThat(from.getRequestStartLine().getMethod()).isNotNull();
-        assertThat(from.getRequestStartLine().getPath()).isNotNull();
-        assertThat(from.getRequestStartLine().getProtocol()).isNotNull();
-        assertThat(from.getHttpHeaders()).isNotNull();
+        assertAll(() -> assertThat(from.getRequestStartLine().getMethod()).isNotNull(),
+                () -> assertThat(from.getRequestStartLine().getPath()).isNotNull(),
+                () -> assertThat(from.getRequestStartLine().getProtocol()).isNotNull(),
+                () -> assertThat(from.getHttpHeaders()).isNotNull(),
+                () -> assertThat(from.getRequestBody()).isNull(),
+                () -> assertThat(from.getHttpHeaders().getHeader("Host")).isEqualTo("www.example.com"));
     }
 
     @Test
@@ -38,13 +46,13 @@ class HttpRequestTest {
                 "Content-Length: 13\r\n" +
                 "\r\n" + "Hello, World!";
 
-        HttpRequest from = HttpRequest.from(new BufferedReader(new StringReader(input)));
+        HttpRequest from = getHttpRequest(input);
 
-        assertThat(from.getRequestStartLine().getMethod()).isEqualTo(HttpMethod.POST);
-        assertThat(from.getRequestStartLine().getPath()).isEqualTo("/index.html");
-        assertThat(from.getRequestStartLine().getProtocol()).isEqualTo("HTTP/1.1");
-        assertThat(from.getHttpHeaders()).isNotNull();
-        assertThat(from.getRequestBody().toString()).contains("Hello, World!");
+        assertAll(() -> assertThat(from.getRequestStartLine().getMethod()).isEqualTo(HttpMethod.POST),
+                () -> assertThat(from.getRequestStartLine().getPath()).isEqualTo("/index.html"),
+                () -> assertThat(from.getRequestStartLine().getProtocol()).isEqualTo("HTTP/1.1"),
+                () -> assertThat(from.getHttpHeaders()).isNotNull(),
+                () -> assertThat(from.getRequestBody().toString()).contains("Hello, World!"));
     }
 
     @Test
@@ -56,13 +64,32 @@ class HttpRequestTest {
                 "Content-Length: 113\r\n" +
                 "\r\n" + "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
 
-        HttpRequest from = HttpRequest.from(new BufferedReader(new StringReader(input)));
+        HttpRequest from = getHttpRequest(input);
 
-        assertThat(from.getRequestStartLine().getMethod()).isEqualTo(HttpMethod.POST);
-        assertThat(from.getRequestStartLine().getPath()).isEqualTo("/index.html");
-        assertThat(from.getRequestStartLine().getProtocol()).isEqualTo("HTTP/1.1");
-        assertThat(from.getHttpHeaders()).isNotNull();
-        assertThat(from.getRequestBody().toString()).contains("재성");
+        assertAll(() -> assertThat(from.getRequestStartLine().getMethod()).isEqualTo(HttpMethod.POST),
+                () -> assertThat(from.getRequestStartLine().getPath()).isEqualTo("/index.html"),
+                () -> assertThat(from.getRequestStartLine().getProtocol()).isEqualTo("HTTP/1.1"),
+                () -> assertThat(from.getHttpHeaders()).isNotNull(),
+                () -> assertThat(from.getRequestBody().toString()).contains("javajigi"),
+                () -> assertThat(from.getRequestBody().toString()).contains("password"),
+                () -> assertThat(from.getRequestBody().toString()).contains("박재성"));
+    }
+
+    @Test
+    @DisplayName("HttpRequest 객체 생성 테스트 - body가 있는 경우, 쿼리 스트링이 있는 경우")
+    void create_all() throws IOException {
+        String input = "GET /create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1\r\n" +
+                "Host: www.example.com\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: 0\r\n";
+
+        HttpRequest from = getHttpRequest(input);
+
+        assertAll(() -> assertThat(from.getRequestStartLine().getMethod()).isEqualTo(HttpMethod.GET),
+                () -> assertThat(from.getRequestStartLine().getQueryString().get("userId")).isEqualTo("javajigi"),
+                () -> assertThat(from.getRequestStartLine().getQueryString().get("password")).isEqualTo("password"),
+                () -> assertThat(from.getRequestStartLine().getQueryString().get("name")).isEqualTo("박재성"),
+                () -> assertThat(from.getRequestStartLine().getQueryString().get("email")).isEqualTo("javajigi@slipp.net"));
     }
 
 }
