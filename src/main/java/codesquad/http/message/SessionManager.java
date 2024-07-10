afc4a213fecaf06582c53039f75743d46c69a693
HyeonJun0530 <hyjn053@gmail.com>
2024-07-10 10:47:02 +0900

    feat: 세션을 관리하는 세션 매니저 구현
    - 세션 추가, 삭제, 확인, 유저 정보 가져오기 구현
    - 세션 클래스에 대한 테스트 작성

---
+++ src/main/java/codesquad/http/message/SessionManager.java
@@ -0,0 +1,41 @@
+package codesquad.http.message;
+
+import codesquad.app.domain.User;
+import codesquad.app.repository.UserRepository;
+import codesquad.http.message.response.HttpResponse;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private static final Map<String, User> sessions = new ConcurrentHashMap<>();
+
+    private SessionManager() {
+    }
+
+    public static String createSession(final String userId, final HttpResponse response) {
+        UUID sessionId = UUID.randomUUID();
+        User user = UserRepository.findByUserId(userId);
+        sessions.put(sessionId.toString(), user);
+
+        response.setCookie(new Cookie("SID", sessionId.toString()));
+
+        return sessionId.toString();
+    }
+
+    public static Optional<User> getUserId(final String sessionId) {
+        return Optional.ofNullable(sessions.get(sessionId));
+    }
+
+    public static void removeSession(final String sessionId) {
+        sessions.remove(sessionId);
+    }
+
+    public static boolean isValidSession(final String sessionId) {
+        return sessions.containsKey(sessionId);
+    }
+
+}
---
+++ src/test/java/codesquad/http/message/SessionManagerTest.java
@@ -0,0 +1,81 @@
+package codesquad.http.message;
+
+import codesquad.app.domain.User;
+import codesquad.app.repository.UserRepository;
+import codesquad.http.message.constant.HttpStatus;
+import codesquad.http.message.response.HttpResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    private static User save() {
+        User user = new User.Builder()
+                .name("name")
+                .email("email")
+                .password("password")
+                .userId("userId")
+                .build();
+        UserRepository.save(user);
+        return user;
+    }
+
+    @Test
+    @DisplayName("세션을 생성한다.")
+    void createSession() {
+        User user = save();
+
+        HttpResponse httpResponse = HttpResponse.of("HTTP1.1", HttpStatus.OK);
+        SessionManager.createSession(user.getUserId(), httpResponse);
+
+        assertTrue(httpResponse.toString().contains("Set-Cookie: SID="));
+    }
+
+    @Test
+    @DisplayName("세션을 통해 유저 아이디를 가져온다.")
+    void getUserId() {
+        User user = save();
+
+        HttpResponse httpResponse = HttpResponse.of("HTTP1.1", HttpStatus.OK);
+        String session = SessionManager.createSession(user.getUserId(), httpResponse);
+
+        Optional<User> userId = SessionManager.getUserId(session);
+
+        assertAll(
+                () -> assertTrue(userId.isPresent()),
+                () -> assertTrue(userId.get().getUserId().equals(user.getUserId())));
+    }
+
+    @Test
+    @DisplayName("세션을 제거한다.")
+    void removeSession() {
+        User user = save();
+
+        HttpResponse httpResponse = HttpResponse.of("HTTP1.1", HttpStatus.OK);
+        String session = SessionManager.createSession(user.getUserId(), httpResponse);
+
+        SessionManager.removeSession(session);
+
+        assertTrue(SessionManager.getUserId(session).isEmpty());
+    }
+
+    @Test
+    @DisplayName("세션이 유효한지 확인한다.")
+    void isValidSession() {
+        User user = save();
+
+        HttpResponse httpResponse = HttpResponse.of("HTTP1.1", HttpStatus.OK);
+        String session = SessionManager.createSession(user.getUserId(), httpResponse);
+
+        assertTrue(SessionManager.isValidSession(session));
+    }
+
+    @Test
+    void isValidSessionWithInvalidSession() {
+        assertFalse(SessionManager.isValidSession("invalidSession"));
+    }
+}
