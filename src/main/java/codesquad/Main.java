c09113447358b63dd999e9dc0472b0976409ed50
HyeonJun0530 <hyjn053@gmail.com>
2024-07-12 11:28:33 +0900

    fix: main에서 UserDataBase를 import하던 버그 수정

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,7 +1,5 @@
 package codesquad;
 
-import codesquad.app.domain.User;
-import codesquad.app.infrastructure.UserDataBase;
 import codesquad.config.ExecutorServiceConfiguration;
 import codesquad.http.HttpProcessor;
 import org.slf4j.Logger;
@@ -15,37 +13,22 @@ import java.util.concurrent.ExecutorService;
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static final int DEFAULT_PORT = 8080;
 
     /**
-     * TODO: Byte 끌어올릴 때 크기 체크
-     **/
+     * TODD: Exception 구체화
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        logger.info("Server is starting...");
+        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
+        logger.info("Listening for connection on port 8080 ....");
 
-        try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
-            init();
-            logger.info("Listening for connection on port 8080 ....");
+        ExecutorService executorService = ExecutorServiceConfiguration.getExecutorService();
 
-            ExecutorService executorService = ExecutorServiceConfiguration.getExecutorService();
-
-            while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-                Socket clientSocket = serverSocket.accept(); // 클라이언트 연결을 수락합니다.
-                executorService.submit(new HttpProcessor(clientSocket)); // 클라이언트 요청을 병렬로 처리합니다.
-            }
-        } // 8080 포트에서 서버를 엽니다.
-    }
-
-    public static void init() {
-        for (int i = 0; i < 10; i++) {
-            User user = new User.Builder()
-                    .userId("dummy" + i)
-                    .name("dummyName" + i)
-                    .password("dummyPassword" + i)
-                    .email("dummyEmail" + i + "@test.com")
-                    .build();
-
-            UserDataBase.save(user);
+        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
+            Socket clientSocket = serverSocket.accept(); // 클라이언트 연결을 수락합니다.
+            executorService.submit(new HttpProcessor(clientSocket)); // 클라이언트 요청을 병렬로 처리합니다.
         }
     }
 }
