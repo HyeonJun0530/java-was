c8891afd81e1a70dba3a61b6c7c658be6c6394fa
HyeonJun0530 <hyjn053@gmail.com>
2024-07-16 23:44:47 +0900

    feat: H2를 통한 데이터 소스 설정 및 커넥션 풀 생성
    - 커넥션 풀 생성 구현
    - 데이터 소스 반환

---
+++ src/main/java/codesquad/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package codesquad.config;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+
+import javax.sql.DataSource;
+
+public class DataSourceConfig {
+
+    private static final String URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String USER_NAME = "sa";
+    private static final String PASSWORD = "";
+
+    private final DataSource dataSource;
+
+    public DataSourceConfig() {
+        JdbcConnectionPool connectionPool = JdbcConnectionPool.create(URL, USER_NAME, PASSWORD);
+        ;
+        connectionPool.setMaxConnections(10);
+
+        this.dataSource = connectionPool;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+
+}
